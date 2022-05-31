829e54784fd7671d43ee5bba52709e407733796f
guillermorangel <guillermo.m.rangel@gmail.com>
2022-05-30 20:57:35 -0400

    use state hook

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,12 +3,14 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting(props) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  // const name = ''
+  const [name, setName] = React.useState(props.intialName ?? '')
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
@@ -23,7 +25,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting intialName="initial name"/>
 }
 
 export default App
